d2357418a7fababae23942e3169f5fa774f848fd
Dan-Eric Næss <dan-eric.naess@live.no>
2012-11-08 19:56:34 +0100

    Better gui for appointment
    that

--- gooey/ui_widget.h
+++ gooey/ui_widget.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'widget.ui'
 **
-** Created: Wed 7. Nov 18:39:49 2012
+** Created: Thu 8. Nov 18:49:00 2012
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -20,6 +20,7 @@
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
 #include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
 #include <QtGui/QRadioButton>
 #include <QtGui/QTextEdit>
 #include <QtGui/QVBoxLayout>
@@ -30,7 +31,7 @@ QT_BEGIN_NAMESPACE
 class Ui_Widget
 {
 public:
-    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
     QVBoxLayout *verticalLayout_2;
     QHBoxLayout *horizontalLayout_2;
     QLabel *label;
@@ -41,11 +42,11 @@ public:
     QHBoxLayout *horizontalLayout_6;
     QLabel *label_5;
     QDateTimeEdit *dateTimeEnd;
-    QVBoxLayout *verticalLayout;
-    QRadioButton *radioMote;
+    QHBoxLayout *horizontalLayout_7;
     QRadioButton *radioUndervisning;
     QRadioButton *radioButton_3;
-    QRadioButton *radioButton_4;
+    QRadioButton *radioMote;
+    QVBoxLayout *verticalLayout;
     QHBoxLayout *horizontalLayout;
     QRadioButton *radioAnnet;
     QLineEdit *lineEditAnnet;
@@ -55,6 +56,7 @@ public:
     QHBoxLayout *horizontalLayout_3;
     QLabel *label_3;
     QLineEdit *lineEdit_3;
+    QPushButton *pushButton;
     QTextEdit *textEdit;
     QDialogButtonBox *buttonBox;
 
@@ -62,8 +64,8 @@ public:
     {
         if (Widget->objectName().isEmpty())
             Widget->setObjectName(QString::fromUtf8("Widget"));
-        Widget->resize(279, 512);
-        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        Widget->resize(276, 439);
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
         sizePolicy.setHeightForWidth(Widget->sizePolicy().hasHeightForWidth());
@@ -72,23 +74,22 @@ public:
         icon.addFile(QString::fromUtf8("../../../Dropbox/Public/fv.png"), QSize(), QIcon::Normal, QIcon::Off);
         Widget->setWindowIcon(icon);
         Widget->setWindowOpacity(1);
-        widget = new QWidget(Widget);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(10, 20, 258, 485));
-        verticalLayout_2 = new QVBoxLayout(widget);
+        verticalLayout_3 = new QVBoxLayout(Widget);
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setSpacing(6);
-        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
-        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setSpacing(6);
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
-        label = new QLabel(widget);
+        label = new QLabel(Widget);
         label->setObjectName(QString::fromUtf8("label"));
 
         horizontalLayout_2->addWidget(label);
 
-        appointmentName = new QLineEdit(widget);
+        appointmentName = new QLineEdit(Widget);
         appointmentName->setObjectName(QString::fromUtf8("appointmentName"));
 
         horizontalLayout_2->addWidget(appointmentName);
@@ -99,12 +100,12 @@ public:
         horizontalLayout_5 = new QHBoxLayout();
         horizontalLayout_5->setSpacing(6);
         horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
-        label_4 = new QLabel(widget);
+        label_4 = new QLabel(Widget);
         label_4->setObjectName(QString::fromUtf8("label_4"));
 
         horizontalLayout_5->addWidget(label_4);
 
-        dateTimeStart = new QDateTimeEdit(widget);
+        dateTimeStart = new QDateTimeEdit(Widget);
         dateTimeStart->setObjectName(QString::fromUtf8("dateTimeStart"));
 
         horizontalLayout_5->addWidget(dateTimeStart);
@@ -115,12 +116,12 @@ public:
         horizontalLayout_6 = new QHBoxLayout();
         horizontalLayout_6->setSpacing(6);
         horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
-        label_5 = new QLabel(widget);
+        label_5 = new QLabel(Widget);
         label_5->setObjectName(QString::fromUtf8("label_5"));
 
         horizontalLayout_6->addWidget(label_5);
 
-        dateTimeEnd = new QDateTimeEdit(widget);
+        dateTimeEnd = new QDateTimeEdit(Widget);
         dateTimeEnd->setObjectName(QString::fromUtf8("dateTimeEnd"));
 
         horizontalLayout_6->addWidget(dateTimeEnd);
@@ -128,39 +129,41 @@ public:
 
         verticalLayout_2->addLayout(horizontalLayout_6);
 
+        horizontalLayout_7 = new QHBoxLayout();
+        horizontalLayout_7->setSpacing(6);
+        horizontalLayout_7->setObjectName(QString::fromUtf8("horizontalLayout_7"));
+        radioUndervisning = new QRadioButton(Widget);
+        radioUndervisning->setObjectName(QString::fromUtf8("radioUndervisning"));
+
+        horizontalLayout_7->addWidget(radioUndervisning);
+
+        radioButton_3 = new QRadioButton(Widget);
+        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
+
+        horizontalLayout_7->addWidget(radioButton_3);
+
+        radioMote = new QRadioButton(Widget);
+        radioMote->setObjectName(QString::fromUtf8("radioMote"));
+
+        horizontalLayout_7->addWidget(radioMote);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_7);
+
         verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(6);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        radioMote = new QRadioButton(widget);
-        radioMote->setObjectName(QString::fromUtf8("radioMote"));
-
-        verticalLayout->addWidget(radioMote);
-
-        radioUndervisning = new QRadioButton(widget);
-        radioUndervisning->setObjectName(QString::fromUtf8("radioUndervisning"));
-
-        verticalLayout->addWidget(radioUndervisning);
-
-        radioButton_3 = new QRadioButton(widget);
-        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
-
-        verticalLayout->addWidget(radioButton_3);
-
-        radioButton_4 = new QRadioButton(widget);
-        radioButton_4->setObjectName(QString::fromUtf8("radioButton_4"));
-
-        verticalLayout->addWidget(radioButton_4);
-
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setSpacing(6);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        radioAnnet = new QRadioButton(widget);
+        radioAnnet = new QRadioButton(Widget);
         radioAnnet->setObjectName(QString::fromUtf8("radioAnnet"));
 
         horizontalLayout->addWidget(radioAnnet);
 
-        lineEditAnnet = new QLineEdit(widget);
+        lineEditAnnet = new QLineEdit(Widget);
         lineEditAnnet->setObjectName(QString::fromUtf8("lineEditAnnet"));
+        lineEditAnnet->setEnabled(false);
 
         horizontalLayout->addWidget(lineEditAnnet);
 
@@ -173,12 +176,12 @@ public:
         horizontalLayout_4 = new QHBoxLayout();
         horizontalLayout_4->setSpacing(6);
         horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
-        label_2 = new QLabel(widget);
+        label_2 = new QLabel(Widget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
 
         horizontalLayout_4->addWidget(label_2);
 
-        lineEdit_2 = new QLineEdit(widget);
+        lineEdit_2 = new QLineEdit(Widget);
         lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
 
         horizontalLayout_4->addWidget(lineEdit_2);
@@ -189,69 +192,69 @@ public:
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setSpacing(6);
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-        label_3 = new QLabel(widget);
+        label_3 = new QLabel(Widget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
 
         horizontalLayout_3->addWidget(label_3);
 
-        lineEdit_3 = new QLineEdit(widget);
+        lineEdit_3 = new QLineEdit(Widget);
         lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+        lineEdit_3->setEnabled(false);
 
         horizontalLayout_3->addWidget(lineEdit_3);
+
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setEnabled(true);
+        QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy1);
+        pushButton->setMaximumSize(QSize(20, 23));
+        pushButton->setMouseTracking(false);
+
+        horizontalLayout_3->addWidget(pushButton);
 
 
         verticalLayout_2->addLayout(horizontalLayout_3);
 
-        textEdit = new QTextEdit(widget);
+        textEdit = new QTextEdit(Widget);
         textEdit->setObjectName(QString::fromUtf8("textEdit"));
 
         verticalLayout_2->addWidget(textEdit);
 
-        buttonBox = new QDialogButtonBox(widget);
+        buttonBox = new QDialogButtonBox(Widget);
         buttonBox->setObjectName(QString::fromUtf8("buttonBox"));
         buttonBox->setMaximumSize(QSize(16777215, 16777215));
         buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
 
         verticalLayout_2->addWidget(buttonBox);
 
-        appointmentName->raise();
-        dateTimeStart->raise();
-        dateTimeEnd->raise();
-        lineEdit_2->raise();
-        lineEdit_3->raise();
-        textEdit->raise();
-        buttonBox->raise();
-        radioMote->raise();
-        radioUndervisning->raise();
-        radioButton_3->raise();
-        radioButton_4->raise();
-        radioAnnet->raise();
-        lineEditAnnet->raise();
-        textEdit->raise();
-        label_2->raise();
-        label_3->raise();
-        label_4->raise();
-        label_5->raise();
+
+        verticalLayout_3->addLayout(verticalLayout_2);
+
 
         retranslateUi(Widget);
         QObject::connect(buttonBox, SIGNAL(clicked(QAbstractButton*)), Widget, SLOT(close()));
+        QObject::connect(radioAnnet, SIGNAL(toggled(bool)), lineEditAnnet, SLOT(setEnabled(bool)));
 
         QMetaObject::connectSlotsByName(Widget);
     } // setupUi
 
     void retranslateUi(QWidget *Widget)
     {
-        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", 0, QApplication::UnicodeUTF8));
+        Widget->setWindowTitle(QApplication::translate("Widget", "Nytt arrangement", 0, QApplication::UnicodeUTF8));
         label->setText(QApplication::translate("Widget", "Navn", 0, QApplication::UnicodeUTF8));
         label_4->setText(QApplication::translate("Widget", "Start", 0, QApplication::UnicodeUTF8));
         label_5->setText(QApplication::translate("Widget", "Slutt", 0, QApplication::UnicodeUTF8));
-        radioMote->setText(QApplication::translate("Widget", "M\303\270te", 0, QApplication::UnicodeUTF8));
         radioUndervisning->setText(QApplication::translate("Widget", "Undervisning", 0, QApplication::UnicodeUTF8));
         radioButton_3->setText(QApplication::translate("Widget", "Laboratorium", 0, QApplication::UnicodeUTF8));
-        radioButton_4->setText(QApplication::translate("Widget", "RadioButton", 0, QApplication::UnicodeUTF8));
+        radioMote->setText(QApplication::translate("Widget", "M\303\270te", 0, QApplication::UnicodeUTF8));
         radioAnnet->setText(QApplication::translate("Widget", "Annet", 0, QApplication::UnicodeUTF8));
         label_2->setText(QApplication::translate("Widget", "Sted", 0, QApplication::UnicodeUTF8));
         label_3->setText(QApplication::translate("Widget", "Kontakter", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("Widget", "..", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
